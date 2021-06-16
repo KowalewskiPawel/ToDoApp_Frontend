bcf3576e44d96f515f82216a99b25e6de7a07eab
KowalewskiPawel <pawelkowalewsk@gmail.com>
2021-06-16 20:48:45 +0300

    todos

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,18 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import userService from "../services/user.service";
 
 function ToDoList() {
+  const [todos, setTodos] = useState([]);
+
   useEffect(() => {
     userService.getTodos().then(
       (response) => {
         console.log(response.data);
+        setTodos([...response.data]);
       },
       (err) => {
         console.error(err);
       }
     );
-  });
+  }, []);
 
   return (
     <div className="todoForm">
@@ -24,6 +27,25 @@ function ToDoList() {
         <option value="Completed">Completed Tasks</option>
         <option value="Uncompleted">Uncompleted Tasks</option>
       </select>
+
+      {todos.length < 1 ? (
+        <p>There are no To-Do lists</p>
+      ) : (
+        todos.map((todo, index) => {
+          let tempDate = new Intl.DateTimeFormat("en-GB").format(
+            new Date(todo.published_at)
+          );
+          let date = tempDate.split("/").join("-");
+          for (let tasks of todo.task)
+            return (
+              <div key={index}>
+                <span>{todo.name}</span>
+                <em>Created at: {date}</em>
+                <span>All: {todo.task.length}</span>
+              </div>
+            );
+        })
+      )}
     </div>
   );
 }
